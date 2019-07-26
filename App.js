bb44cd2fac6ab8a9d1b128de98977debb9dbdbc0
merlox <merunasgrincalaitis@gmail.com>
2019-07-26 13:49:45 +0200

    Added mockup

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import styles from './src/styles'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+    constructor() {super()}
+
+    render() {
+        return (
+            <View style={styles.mainContainer}>
+                <Text style={styles.mainTitle}>Crypto Ticker</Text>
+            </View>
+        )
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+    mainTitle: {
+        color: 'white',
+        fontFamily: "Lato-Black",
+    },
+    mainContainer: {
+        flex: 1,
+        backgroundColor: '#697b89',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
+
+export default styles
